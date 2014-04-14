87d15d2083af9ceaa6d13ef0d6e2eb3c75ebfd41
Oliver Grimm <oliver.grimm@bluemail.ch>
2014-04-14 21:48:58 +0200

    update catch07

--- jppcp_kml.js
+++ jppcp_kml.js
@@ -87,8 +87,17 @@ var gpx6 = new ol.layer.Vector({
     return gpsstyle1[feature.getGeometry().getType()];
   }
 });
+var gpx7 = new ol.layer.Vector({
+  source: new ol.source.GPX({
+    projection: 'EPSG:3857',
+    url: 'data/tracs/trac20140413.gpx'
+  }),
+  style: function(feature, resolution) {
+    return gpsstyle1[feature.getGeometry().getType()];
+  }
+});
 var map = new ol.Map({
-  layers: [raster,vector1,vector2,gpx1,gpx2,gpx3,gpx4,gpx5,gpx6],
+  layers: [raster,vector1,vector2,gpx1,gpx2,gpx3,gpx4,gpx5,gpx6,gpx7],
   renderer: 'canvas',
   target: document.getElementById('map'),
   view: new ol.View2D({
